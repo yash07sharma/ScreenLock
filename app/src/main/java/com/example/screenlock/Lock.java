d826d71473a6fe29482791ebd01e817e60c3ba42
yash07sharma <yash2307sharma@gmail.com>
2020-03-11 14:47:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/screenlock/Lock.java
@@ -0,0 +1,168 @@
+package com.example.screenlock;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Pair;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class Lock extends View {
+    private int primaryColor;
+    private Paint paint;
+    private List<Float> X,Y;
+    private int centersTouched;
+    private Pair<Float,Float> current;
+    private int choice;
+    private boolean touched[][]=new boolean[3][3];
+    private String pattern,password;
+    public Lock(Context context) {
+        super(context);
+        init();
+    }
+
+    public Lock(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public Lock(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        // Load attributes
+        primaryColor=Color.DKGRAY;
+        choice=0;
+        paint=new Paint();
+        X=new ArrayList<>();
+        Y=new ArrayList<>();
+        centersTouched=0;
+        current=new Pair<>(0f,0f);
+        password="03678";//hard-coded pattern for password
+        pattern="";
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+                touched[i][j]=false;
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                paint.setColor(primaryColor);
+                canvas.drawCircle(100f+i*200f,100f+j*200f,40f,paint);
+                paint.setColor(Color.WHITE);
+                canvas.drawCircle(100f+200f*i,100f+200f*j,20f,paint);
+            }
+        }
+        paint.setColor(primaryColor);
+        for(int i=0;i<centersTouched;i++)//Touched centers
+            canvas.drawCircle(X.get(i),Y.get(i),20f,paint);
+
+        paint.setStrokeWidth(20f);
+        for(int i=0;i<centersTouched-1;i++)//Lines b/w touched centers
+            canvas.drawLine(X.get(i),Y.get(i),X.get(i+1),Y.get(i+1),paint);
+
+        switch(choice){
+            case 1:
+                if(centersTouched>=2) canvas.drawLine(X.get(centersTouched-2),Y.get(centersTouched-2),X.get(centersTouched-1),Y.get(centersTouched-1),paint);
+                break;
+            case 2:
+                if(centersTouched>0) canvas.drawLine(X.get(centersTouched-1),Y.get(centersTouched-1),current.first,current.second,paint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        int action=motionEvent.getAction();
+        float x=motionEvent.getX();
+        float y=motionEvent.getY();
+
+        float nearX=(((int)x)/200)*200+100f;
+        float nearY=(((int)y)/200)*200+100f;
+        float dist=(float)Math.sqrt(Math.pow(nearX-x,2)+(float)Math.pow(nearY-y,2));
+
+        switch(action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                if(x>600f || y>600f)
+                    return true;
+                else if(dist<=40 && !touched[((int)nearX-100)/200][((int)nearY-100)/200])
+                {
+                    choice=1;
+                    touched[((int)nearX-100)/200][((int)nearY-100)/200]=true;
+                    X.add(nearX);Y.add(nearY);
+                    centersTouched++;
+                    pattern+=((((int)nearX-100)*3/200)+((int)nearY-100)/200);
+                    invalidate();
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                choice=0;
+                if(pattern.equals(password)&&centersTouched>0){
+                    primaryColor=Color.GREEN;
+                    invalidate();
+                    Toast.makeText(getContext(),"Correct !!",Toast.LENGTH_SHORT).show();
+                }
+                else if(centersTouched>0){
+                    primaryColor=Color.RED;
+                    invalidate();
+                    Toast.makeText(getContext(),"Incorrect !!",Toast.LENGTH_SHORT).show();
+                }
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        init();
+                        invalidate();
+                    }
+                },3000);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if(dist<=40 && x<=600f && y<=600f) {
+                    if( !touched[((int)nearX-100)/200][((int)nearY-100)/200]) {
+                        touched[((int) nearX - 100) / 200][((int) nearY - 100) / 200] = true;
+                        X.add(nearX);Y.add(nearY);
+                        pattern+=((((int)nearX-100)/200)+((int)nearY-100)*3/200);
+                        centersTouched++;
+
+                    }
+                    choice=1;
+                }
+                else {
+                    current=new Pair<>(x,y);
+                    choice = 2;
+                }
+                invalidate();
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/screenlock/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.screenlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.GridLayout;
+
+public class MainActivity extends AppCompatActivity {
+    Lock lock;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        lock=findViewById(R.id.lock);
+    }
+}
